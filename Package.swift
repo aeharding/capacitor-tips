afd4e183b5166fb82c5e23820915ba67d93e77eb
Alexander Harding <noreply@harding.dev>
2024-12-19 23:45:48 -0600

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapacitorTips",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "CapacitorTips",
+            targets: ["CapacitorTipsPlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "6.0.0")
+    ],
+    targets: [
+        .target(
+            name: "CapacitorTipsPlugin",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm")
+            ],
+            path: "ios/Sources/CapacitorTipsPlugin"),
+        .testTarget(
+            name: "CapacitorTipsPluginTests",
+            dependencies: ["CapacitorTipsPlugin"],
+            path: "ios/Tests/CapacitorTipsPluginTests")
+    ]
+)
